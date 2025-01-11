e84106998081968b92ee3a0fc817fb71d8d678b5
Vlad <vlad_vlasov98@mail.ru>
2025-01-11 18:00:32 +0300

    elk and inference compl

--- deployment/backend/app/main.py
+++ deployment/backend/app/main.py
@@ -1,35 +1,52 @@
-# Главный файл для запуска FastAPI-сервиса.
-
-import uvicorn
+import logging
+from logging.handlers import SocketHandler
+from pythonjsonlogger import jsonlogger
 from fastapi import FastAPI
-from deployment.backend.app.routers import api
-
-# import logging
-# from logging.handlers import TimedRotatingFileHandler
-
-# # Configure logging
-# log_handler = TimedRotatingFileHandler("logs/app.log", when="midnight", interval=1)
-# log_handler.suffix = "%Y%m%d"
-# logging.basicConfig(
-#     level=logging.INFO,
-#     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-#     handlers=[log_handler],
-# )
-# logger = logging.getLogger(__name__)
+import uvicorn
 
 
+LOGSTASH_HOST = "logstash"
+LOGSTASH_PORT = 5000
 
 
-app = FastAPI(title="ML Model Management API",
-              docs_url="/api/openapi",
-              version="1.0")
+logger = logging.getLogger("backend_logger")
+logger.setLevel(logging.INFO)
 
-# Include API router
-app.include_router(api.router, prefix="/api/v1", tags=["ML API"])
+logstash_handler = SocketHandler(LOGSTASH_HOST, LOGSTASH_PORT)
+formatter = jsonlogger.JsonFormatter(fmt="%(asctime)s %(name)s %(levelname)s %(message)s %(service)s %(module)s")
+logstash_handler.setFormatter(formatter)
+logger.addHandler(logstash_handler)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
+
+app = FastAPI(
+    title="ML Model Management API",
+    docs_url="/api/openapi",
+    version="1.0"
+)
 
 @app.get("/", tags=["Health Check"])
 def read_root():
+    logger.info(
+        "Health check endpoint was called",
+        extra={"service": "backend", "module": "health_check"}
+    )
     return {"message": "ML API is running!"}
 
+@app.get("/log", tags=["Logging"])
+def test_logging():
+    logger.info(
+        "Test logging endpoint was called",
+        extra={"service": "backend", "module": "test_logging"}
+    )
+    return {"message": "Log sent to Logstash!"}
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    logger.info(
+        "Starting FastAPI service",
+        extra={"service": "backend", "module": "startup"}
+    )
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
